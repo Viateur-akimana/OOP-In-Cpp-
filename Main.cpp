ed6f88e00215a534e3fc5c6ac601a0abf6e10adf
Viateur <akimanaviateur94@gmail.com>
2023-10-30 08:44:16 +0100

    inheritance in c++

--- Main.cpp
+++ Main.cpp
@@ -1,38 +1,54 @@
-#include<iostream>;
-    using namespace std;
+#include<iostream>
+using namespace std;
 
-    class Employee {
-    private:
-        string name;
-        string company;
-        int age;
-        public:
-        void setName(string Name){
+class Employee {
+protected:
+    string company;
+    string name;
+    int age;
+public:
+    void setName(string Name) {
         name = Name;
-        };
-        string getName(){
-        return name;
-        }
-         void setCompany(string Company){
-                company = Company;
-                };
-          string getCompany(){
-                return company;
-                };
-          void setAge(int Age){
-          age = Age;
-          }
-          int getAge(){
-          return age;
-          }
+
     };
-
-    int main() {
-        Employee employee1;
-        employee1.setName("Viateur");
-        employee1.setCompany("RCA");
-        employee1.setAge(45);
-        cout << "MY name is" << employee1.getName() << "And i work for" << employee1.getCompany() << "I am " << employee1.getAge();
-
-        return 0; // Indicates successful execution of the program
+    string getName() {
+        return name;
     }
+    void setCompany(string Company) {
+        company = Company;
+    };
+    string getCompany() {
+        return company;
+    }
+    void setAge(int Age) {
+        age = Age;
+    }
+    int getAge() {
+        return age;
+    }
+    Employee(string name, string company, int age){
+    name= name;
+    company = company;
+    age = age;
+    }
+};
+
+class Developer : public Employee {
+public:
+    string language;
+    Developer(string name, string company, int age, string language)
+        : Employee(name, company, age) // Initialize the base class constructor using an initialization list
+    {
+        this->language = language; // Use 'this->' to refer to the member variable of the class
+    }
+
+    void favoriteLanguage() {
+        cout << "My name is " << name<< ". My favorite language is " <<language << endl;
+    }
+};
+
+int main() {
+    Developer d = Developer("viateur", "RCA", 89, "c++");
+    d.favoriteLanguage();
+    return 0; // Indicates successful execution of the program
+}
